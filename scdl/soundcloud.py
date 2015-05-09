fda1a9388ea70a84ecd72238136674ec71be06ac
David Fischer <david.fischer.ch@gmail.com>
2015-05-09 22:53:36 +0000

    Fix get_all when offset not equal to 0

--- scdl/soundcloud.py
+++ scdl/soundcloud.py
@@ -7,10 +7,10 @@ class Client(soundcloud.Client):
 
     def get_all(self, url, offset=0, limit=200, **kwargs):
         resources = set()
-        prev_offset = None
+        prev_offset, start_offset = None, offset
         while offset != prev_offset:
             resources.update(self.get(url, offset=offset, limit=limit, **kwargs))
-            prev_offset, offset = offset, len(resources)
+            prev_offset, offset = offset, start_offset + len(resources)
         return resources
 
 resource = soundcloud.resource
