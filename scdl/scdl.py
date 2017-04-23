c27ff602a641b933ee97c85de3f89b9a3a7661e9
Reid Pryzant <rapigan@gmail.com>
2017-04-23 08:30:07 -0700

    Update scdl.py

--- scdl/scdl.py
+++ scdl/scdl.py
@@ -443,7 +443,7 @@ def download_track(track, playlist_name=None, playlist_file=None):
         shutil.move(temp.name, os.path.join(os.getcwd(), filename))
         if filename.endswith('.mp3') or filename.endswith('.m4a'):
             try:
-                settings(track, filename, playlist_name)
+                setMetadata(track, filename, playlist_name)
             except Exception as e:
                 logger.error('Error trying to set the tags...')
                 logger.debug(e)
@@ -460,7 +460,7 @@ def download_track(track, playlist_name=None, playlist_file=None):
     logger.info('{0} Downloaded.\n'.format(filename))
 
 
-def settings(track, filename, album=None):
+def setMetadata(track, filename, album=None):
     """
     Set the tags to the mp3
     """
