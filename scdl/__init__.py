63737ebeaafa838b0ce97d421510420c4f9f9883
flyingrub <flyinggrub@gmail.com>
2017-07-17 18:29:45 +0200

    version 1.6.5

--- scdl/__init__.py
+++ scdl/__init__.py
@@ -4,7 +4,7 @@
 
 import os
 
-__version__ = 'v1.6.4'
+__version__ = 'v1.6.5'
 CLIENT_ID = 'a06b61f88c8297cc5abe6167a2d4c519'
 ALT_CLIENT_ID = 'a3e059563d7fd3372b49b37f00a00bcf'
 ALT2_CLIENT_ID = '2t9loNQH90kzJcsFCODdigxfp325aq4z'
