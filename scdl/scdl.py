fc10555c02705a754501602d18a03b7016dbba80
HB4C <nausea@live.jp>
2021-03-30 18:55:12 +0800

    Update scdl.py

--- scdl/scdl.py
+++ scdl/scdl.py
@@ -73,7 +73,7 @@ import configparser
 import mutagen
 
 from mutagen.easymp4 import EasyMP4
-EasyMP4.RegisterTextKey('website', 'www')
+EasyMP4.RegisterTextKey('website', 'website')
 
 from docopt import docopt
 from clint.textui import progress
